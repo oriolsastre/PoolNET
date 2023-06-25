97aaa51a658544f9d3ecffcf5e61700b6ae88c8d
Oriol Sastre <oriol.sastre@gmail.com>
2023-06-25 21:01:29 +0200

    Iniciar testing amb jest i supertest (TS)

---
+++ back/__tests__/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "constants.ts",
+    "constants.example.ts",
+    "jest.config.js",
+  ],
+};
