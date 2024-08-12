91fa6972378162ddbbdc604ea5fb74b51c97bc1a
Oriol Sastre Rienitz <51907013+oriolsastre@users.noreply.github.com>
2024-08-12 18:47:16 +0200

    Mes tests

--- src/__tests__/ReqResTestCase.php
+++ src/__tests__/ReqResTestCase.php
@@ -9,10 +9,22 @@ abstract class ReqResTestCase extends TestCase
 {
     protected Request $req;
     protected Response $res;
+    protected string $testUser = "Test";
+    protected string $testPswd = "Password123!";
 
     protected function newReqRes(): void
     {
         $this->req = new Request();
         $this->res = new Response();
+    }
+    protected function getAssocHeaders(): array
+    {
+        $headers = xdebug_get_headers();
+        $assocHeaders = array();
+        foreach ($headers as $header) {
+            $header = explode(':', $header);
+            $assocHeaders[$header[0]] = $header[1];
+        }
+        return $assocHeaders;
     }
 }
--- src/__tests__/controllers/AuthLoginTest.php
+++ src/__tests__/controllers/AuthLoginTest.php
@@ -22,14 +22,29 @@ class AuthLoginTest extends ReqResTestCase
      * @uses \PoolNET\config\Response
      * @uses \PoolNET\model
      * @uses \PoolNET\User
+     * @uses \PoolNET\service\JwtHandler
      */
     public function testPost()
     {
         $this->newReqRes();
+
+        // Test exit
+        $this->req->body = ["usuari" => $this->testUser, "password" => $this->testPswd];
+        ob_start();
+        AuthLogin::post($this->req, $this->res);
+        $this->assertEquals(200, http_response_code());
+        $headers = $this->getAssocHeaders();
+        $this->assertArrayHasKey('Set-Cookie', $headers);
+        $this->assertStringContainsString('token=', $headers['Set-Cookie']);
+        // TODO: token rebut
+
+        ob_end_clean();
+
         $this->req->body = ["usuari" => "Test", "password" => "Test123"];
         ob_start();
         AuthLogin::post($this->req, $this->res);
         $this->assertEquals(400, http_response_code());
+
         ob_end_clean();
     }
 }
